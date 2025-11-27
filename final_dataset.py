30d84424a2333cb244c36b4574f3ff125c5f35a0
Yannis Ladron <yannis2.ladron@gmail.com>
2025-11-27 16:19:29 +0100

    matrice corrélation

--- final_dataset.py
+++ final_dataset.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, when
 
 output_path = "final_dataset.csv"
 
@@ -61,6 +61,12 @@ df_final = df_joined.select(
 print("\nSchéma du DataFrame FINAL après sélection :")
 df_final.where(col("Tier_Score").isNotNull()).select("Pokemon", "Tier_Score").show()
 df_final = df_final.where(col("Tier_Score").isNotNull()).dropDuplicates().orderBy("Pokedex Number")
+df_final = df_final.drop("Pokedex Number", "Primary Type", "Secondary Type", "Male Ratio", "Female Ratio", "Base Happiness", "Game(s) of Origin", "Health EV", "Attack EV", "Defense EV", "Special Attack EV", "Special Defense EV", "Speed EV", "EV Yield Total", "Experience Growth Total")
+df_final = df_final.withColumn("Experience Growth", 
+                               when(col("Experience Growth") == "Fast", 0)
+                               .when(col("Experience Growth") == "Medium Fast", 1)
+                               .when(col("Experience Growth") == "Medium Slow", 2)
+                               .when(col("Experience Growth") == "Slow", 3))
 df_pandas = df_final.toPandas()
 
 df_pandas.to_csv(output_path, index=False)
